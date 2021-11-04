675645e6c0d395819b5ed35d0c7f5eb724b91ed0
HouBin506 <764858363@qq.com>
2021-11-04 15:34:41 +0800

    去掉progressManager、RetrofitUrlManager的远程依赖，改为本地代码维护，方便以后自行修改

--- app/src/main/java/com/herenit/armsapp/MainActivity.java
+++ app/src/main/java/com/herenit/armsapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.herenit.armsapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.herenit.progressmanager.demo.ProgressManagerActivity;
+import com.herenit.retrofiturlmanager.demo.RetrofitUrlManagerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void RetrofitUrlManager(View view) {
+        startActivity(new Intent(this, RetrofitUrlManagerActivity.class));
+    }
+
+    public void ProgressManager(View view) {
+        startActivity(new Intent(this, ProgressManagerActivity.class));
     }
 }
